dc124f9bd11151daa82497a4a50a2be70ac96931
Mariam Labib <mariamatef@outlook.com>
2022-03-10 20:09:05 +0200

    fix: some enhancements

--- app/app.py
+++ app/app.py
@@ -93,4 +93,4 @@ def viewAllTopics():
         return render_template('all-topics.html', topicArticleDict=topicArticleDict)
         
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
